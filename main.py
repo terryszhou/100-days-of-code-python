f14c688ad8a740362e9c3d1174602d35ee2bdfe1
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-09 15:58:41 -0500

    Day Two complete.

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ def band_name_generator():
     pet = input("What's your pet's name?\n")
     print(f"Your band name could be {city.capitalize()} {pet.capitalize()}.")
 
-band_name_generator()
+# band_name_generator()
+
+# DAY 2: UNDERSTANDING DATA TYPES AND HOW TO MANIPULATE STRINGS
+def tip_calculator():
+    print("Welcome to the tip calculator.")
+    bill = float(input("What was the total bill? $"))
+    tip = float(input("What percentage tip would you like to give? %"))
+    split = float(input("How many people are splitting the bill?\n"))
+    print(f"Each person should pay: ${'{:.2}'.format(((bill+bill*(tip/100))/split))}")
+
+tip_calculator()
