b8d16eac546016fe7a6c79da5b71c8ba02fa4158
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-09 16:20:15 -0500

    day 3 bmi calculator function complete.

--- main.py
+++ main.py
@@ -15,4 +15,19 @@ def tip_calculator():
     split = float(input("How many people are splitting the bill?\n"))
     print(f"Each person should pay: ${'{:.2}'.format(((bill+bill*(tip/100))/split))}")
 
-tip_calculator()
+# tip_calculator()
+
+# DAY 3: CONTROL FLOW AND LOGICAL OPERATORS
+def bmi_calculator():
+    height = float(input("Enter your height in m: "))
+    weight = float(input("Enter your weight in kg: "))
+    bmi = round(weight / height ** 2)
+    
+    print(f"Your BMI is {bmi}.")
+    if bmi < 18.5: print("You are underweight.")
+    elif bmi < 25: print("You have a normal weight.")
+    elif bmi < 30: print("You are overweight.")
+    elif bmi < 35: print("You are obese.")
+    else: print("You are clinically obese.")
+
+bmi_calculator()
