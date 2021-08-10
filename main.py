8ed0267d8012375eaeb037c18a5ca0ed89e29030
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-10 10:10:37 -0500

    Day Five fizzbuzz exercises complete.

--- main.py
+++ main.py
@@ -248,6 +248,19 @@ def even_adder_2():
     print(f"The sum of all even numbers between {low_num} and {high_num} is {total_sum}.")
 
 # even_adder_2()
+
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else: 
+            print(i)
+
+fizzbuzz()
     
 
 
