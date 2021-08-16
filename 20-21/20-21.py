bb906ac99c351b1124b30962d9fbf7b7a8aaa83a
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-16 16:14:14 -0500

    screen update function added, snake moving forwards as a unit.

--- 20-21/20-21.py
+++ 20-21/20-21.py
@@ -1,20 +1,29 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
-
-for i in range(0,3):
-    snek = Turtle(shape="square")
-    snek.color("white")
-    snek.penup()
-    snek.goto(0 - 20*i, 0)
+screen.tracer(0)
 
 def snake_game():
-    pass
+    snek_list = []
+    for i in range(0,3):
+        snek = Turtle(shape="square")
+        snek.color("white")
+        snek.penup()
+        snek.goto(0 - 20*i, 0)
+        snek_list.append(snek)
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(1)
+        for snek in snek_list:
+            snek.forward(20)
 
-# snake_game()
+
+snake_game()
 
 
 screen.exitonclick()
