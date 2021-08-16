20fa9842f77d32904052ef01ed7a0a5be40af696
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-16 15:26:21 -0500

    Etch-A-Sketch exercise complete.

--- 17-20.py
+++ 17-20.py
@@ -105,5 +105,31 @@ def hirsch_painting():
 
 # hirsch_painting()
 
+# DAY 19: INSTANCES, STATE, AND HIGHER ORDER FUNCTIONS
+def move_forward():
+    timmy.forward(10)
+
+def move_backward():
+    timmy.backward(10)
+
+def turn_left():
+    timmy.setheading(timmy.heading() + 10)
+
+def turn_right():
+    timmy.setheading(timmy.heading() - 10)
+
+def clear():
+    timmy.clear()
+    timmy.penup()
+    timmy.home()
+    timmy.pendown()
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
+
 screen.exitonclick()
 
