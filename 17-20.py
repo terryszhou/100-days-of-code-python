44a80b1c6ee6bfe0bfc0fb19174bf9ee78a8ff5c
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-16 13:33:13 -0500

    Day Eighteen spirograph exercise complete.

--- 17-20.py
+++ 17-20.py
@@ -32,6 +32,55 @@ def quiz_game():
     print("You've completed the quiz!")
     print(f"Your final score was {quiz_brain.score}/{len(quiz_brain.question_list)}.")
 
-quiz_game()
+# quiz_game()
 
+# DAY 18: TURTLE & THE GRAPHICAL USER INTERFACE
+from turtle import Turtle, Screen
+from random import random, randrange, choice
+
+timmy = Turtle()
+screen = Screen()
+screen.colormode(255)
+
+def random_color():
+    R = randrange(256)
+    G = randrange(256)
+    B = randrange(256)
+    timmy.color(R,G,B)
+
+def draw_shape(num_sides):
+    timmy.shape("turtle")
+    random_color()
+    angle = 360/num_sides
+    for _ in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
+
+# for shape_side_n in range(3, 10):
+#     draw_shape(shape_side_n)
+
+def random_walk(num_walks):
+    timmy.shape("circle")
+    timmy.speed("fastest")
+    timmy.pensize(10)
+    degree_range = [1,2,3,4]
+    for _ in range(num_walks):
+        random_color()
+        timmy.forward(30)
+        timmy.right(90 * choice(degree_range))
+
+# random_walk(50)
+
+def spirograph(turn_degree):
+    timmy.speed("fastest")
+    for _ in range(int(360/turn_degree)):
+        random_color()
+        timmy.circle(100)
+        timmy.setheading(timmy.heading() + turn_degree)
+
+spirograph(6)
+
+
+
+screen.exitonclick()
 
