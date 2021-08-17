c782beeeaf4e98cd385cfaca4c1760e96b8a554b
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-17 14:10:00 -0500

    Game Over function written.

--- 20-21/main.py
+++ 20-21/main.py
@@ -26,10 +26,15 @@ def snake_game():
         time.sleep(0.1)
         snake.move()
 
-        # Detect collision with food.
+        # Detect collision with food, increase score.
         if snake.head.distance(food) < 15:
             scoreboard.increase_score()
             food.refresh()
+
+        # Detect collision with wall.
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+            game_is_on = False
+            scoreboard.game_over()
 
 snake_game()
 
