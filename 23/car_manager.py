ffc6d15a0f0905a7308a7c251de99142504802a5
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-18 11:31:18 -0500

    Rendered multiple cars at random starting positions, randomize location/color on reaching leftward screen bound.

--- 23/car_manager.py
+++ 23/car_manager.py
@@ -1,19 +1,31 @@
 from turtle import Turtle
-from random import choice
+from random import choice, randint
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-class CarManager(Turtle):
+class CarManager:
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.color(choice(COLORS))
-        self.penup()
-        self.goto(0,0)
-        self.shapesize(stretch_wid=1, stretch_len=2)
+        self.car_list = []
+        self.create_cars()
+
+    def create_cars(self):
+        for i in range(15):
+            self.add_car()
+
+    def add_car(self):
+        car = Turtle(shape="square")
+        car.shapesize(stretch_wid=1, stretch_len=2)
+        car.color(choice(COLORS))
+        car.penup()
+        car.goto(randint(-280,281), randint(-280,281))
+        self.car_list.append(car)
 
     def move(self):
-        new_x = self.xcor() - STARTING_MOVE_DISTANCE
-        self.goto(new_x, self.ycor())
+        for car in self.car_list:
+            new_x = car.xcor() - STARTING_MOVE_DISTANCE
+            car.goto(new_x, car.ycor())
+            if car.xcor() < -310:
+                car.color(choice(COLORS))
+                car.goto(300, randint(-280,281))
