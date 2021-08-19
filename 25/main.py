ec6bf5abfeebba0c7d208baac49fe653a35adbc4
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-19 09:59:55 -0500

    Notes complete through panda introduction.

--- 25/main.py
+++ 25/main.py
@@ -1,10 +1,16 @@
-import csv
+# # METHOD 1: CSV
+# import csv
 
-with open("weather_data.csv") as data:
-    weather_data = csv.reader(data)
-    temperatures = []
-    for row in weather_data:
-        if row[1] == "temp":
-            continue
-        temperatures.append(int(row[1]))
-    print(temperatures)
+# with open("weather_data.csv") as data:
+#     weather_data = csv.reader(data)
+#     temperatures = []
+#     for row in weather_data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+
+# # METHOD 2: PANDAS
+# import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+# print(data["temp"])
