4ffb30c672d0e400a2fc1ea2c3013dd687dd75cf
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-20 14:17:54 -0500

    Day 28 Pomodoro timer cycle complete.

--- 27-28/pomodoro.py
+++ 27-28/pomodoro.py
@@ -10,22 +10,43 @@ FONT = ("Courier", 35, "bold")
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps in (1, 3, 5, 7):
+        count_down(work_sec)
+        title_label.config(text="WORK!")
+    elif reps in (2, 4, 6):
+        count_down(short_break_sec)
+        title_label.config(text="BREAK!", fg=PINK)
+    elif reps == 8:
+        count_down(long_break_sec)
+        title_label.config(text="BREAK!", fg=RED)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
+    global reps
     count_min = floor(count/60)
+    if count_min < 10:
+        count_min = f"0{count_min}"
     count_sec = count % 60
-    if count_sec == 0 or count_sec < 10:
+    if count_sec < 10:
         count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -38,8 +59,8 @@ canvas.create_image(100,112, image=tomato_img)
 timer_text = canvas.create_text(100,135, text="00.00", fill="white", font=FONT)
 canvas.pack()
 
-timer_label = Label(text="Timer", font=FONT, fg=GREEN, bg=YELLOW)
-timer_label.place(x=45, y=-40)
+title_label = Label(text="Timer", font=FONT, fg=GREEN, bg=YELLOW)
+title_label.place(x=45, y=-40)
 
 start_btn = Button(text="START", highlightbackground=YELLOW, fg=PINK, command=start_timer)
 start_btn.place(x=-60, y=230)
