b8fcbf656da2e82b5ca46627e4d925e01f57c055
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-20 12:38:40 -0500

    Day 28 Pomodoro UI complete.

--- 27-28/pomodoro.py
+++ 27-28/pomodoro.py
@@ -27,4 +27,16 @@ canvas.create_image(100,112, image=tomato_img)
 canvas.create_text(100,135, text="00.00", fill="white", font=FONT)
 canvas.pack()
 
+timer_label = Label(text="Timer", font=FONT, fg=GREEN, bg=YELLOW)
+timer_label.place(x=45, y=-40)
+
+start_btn = Button(text="START", highlightbackground=YELLOW, fg=PINK)
+start_btn.place(x=-60, y=230)
+
+reset_btn = Button(text="STOP", highlightbackground=YELLOW, fg=PINK)
+reset_btn.place(x=200, y=230)
+
+check_marks = Label(text="✓", fg=GREEN, bg=YELLOW)
+check_marks.place(x=90, y=230)
+
 window.mainloop()
