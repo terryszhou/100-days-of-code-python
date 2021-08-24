2a34ed73f4cfa7983bb8409bc68e6f8dc95228b7
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-24 10:22:14 -0500

    Dictionary csv data added, randomized word function added.

--- 31/main.py
+++ 31/main.py
@@ -1,36 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice
+import pandas
 
 # -------------------- CONSTANTS -------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
 LANG_FONT = ("Arial", 40, "italic")
 WORD_FONT = ("Arial", 60, "bold")
+word_data = pandas.read_csv("data/chinese_words.csv")
+word_dict = word_data.to_dict(orient="records")
+
+# -------------------- FUNCTIONS -------------------- #
+def next_card():
+    next_word = choice(word_dict)
+    canvas.itemconfig(card_word, text=next_word["character"])
+
 
 # -------------------- UI SETUP -------------------- #
 window = Tk()
 window.title("Chinese Flashcards")
 window.config(width=900, height=800, padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-FLASHCARD_FRONT = PhotoImage(file="card_front.png")
-FLASHCARD_BACK = PhotoImage(file="card_back.png")
-WRONG = PhotoImage(file="wrong.png")
-RIGHT = PhotoImage(file="right.png")
+FLASHCARD_FRONT = PhotoImage(file="images/card_front.png")
+FLASHCARD_BACK = PhotoImage(file="images/card_back.png")
+WRONG = PhotoImage(file="images/wrong.png")
+RIGHT = PhotoImage(file="images/right.png")
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.create_image(400,263, image=FLASHCARD_FRONT)
+# canvas.create_image(400,263, image=FLASHCARD_BACK)
+card_title = canvas.create_text(400,150, text="Chinese", font=LANG_FONT, fill="black")
+card_word = canvas.create_text(400,263, text="", font=WORD_FONT, fill="black")
 canvas.grid(column=0, row=0, columnspan=2)
 
-wrong_button = Button(image=WRONG, highlightbackground=BACKGROUND_COLOR)
+wrong_button = Button(image=WRONG, highlightbackground=BACKGROUND_COLOR, command=next_card)
 wrong_button.grid(column=0, row=1)
-right_button = Button(image=RIGHT, highlightbackground=BACKGROUND_COLOR)
+right_button = Button(image=RIGHT, highlightbackground=BACKGROUND_COLOR, command=next_card)
 right_button.grid(column=1, row=1)
 
-lang_label = Label(text="Chinese", font=LANG_FONT, bg="white", fg="black")
-lang_label.place(x=320, y=150)
-
-word_label = Label(text="的", font=WORD_FONT, bg="white", fg="black")
-word_label.place(x=360, y=263)
+next_card()
 
 window.mainloop()
 
