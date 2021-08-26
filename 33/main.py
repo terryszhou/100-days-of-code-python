a72c17c52cc1bd93bb98d2071695c3917660dd65
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-26 16:02:29 -0500

    Day 33 through rendering long/lat data from ISS API.

---
+++ 33/main.py
@@ -0,0 +1,11 @@
+import requests
+
+def iss_tracker():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    longitude = data["iss_position"]["longitude"]
+    latitude = data["iss_position"]["latitude"]
+    print(longitude, latitude)
+
+iss_tracker()
