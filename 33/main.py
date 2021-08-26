a9fe7ff3f66be1a5c28c8e89d48456d9c3209848
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-26 16:39:21 -0500

    Day 33 through string formatting of sunrise/sunset today.

--- 33/main.py
+++ 33/main.py
@@ -1,4 +1,8 @@
 import requests
+from datetime import datetime
+
+MY_LAT = 30.319920
+MY_LNG = -97.655210
 
 def iss_tracker():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -8,4 +12,23 @@ def iss_tracker():
     latitude = data["iss_position"]["latitude"]
     print(longitude, latitude)
 
-iss_tracker()
+# iss_tracker()
+
+def sunrise_sunset():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LNG,
+        "formatted": 0
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+    sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+    time_now = datetime.now()
+    print(sunrise)
+    print(sunset)
+    print(time_now.hour)
+
+sunrise_sunset()
