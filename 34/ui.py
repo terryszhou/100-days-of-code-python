3633f3dfda3b0dfbb1a6325b323d8aff89b90bf9
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-27 11:12:04 -0500

    Day 34 questions rendered dynamically into UI model

--- 34/ui.py
+++ 34/ui.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -13,7 +15,8 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_text = self.canvas.create_text(
-            150, 125, 
+            (150, 125),
+            width=280,
             text="Some Question Text", 
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
@@ -28,4 +31,11 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=0)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
