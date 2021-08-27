025d96312ea22415a50a59f9d64968f8bef7a8a0
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-27 15:29:44 -0500

    Day 35 Umbrella Tracker exercise complete.

--- 35/main.py
+++ 35/main.py
@@ -5,15 +5,24 @@ import requests
 load_dotenv(find_dotenv())
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
-
-owm_params = {
+OWM_PARAMS = {
     "lat": os.environ.get("MY_LAT"),
     "lon": os.environ.get("MY_LON"),
     "appid": os.environ.get("WEATHER_API_KEY"),
     "exclude": "current,minutely,daily"
 }
 
-res = (requests.get(f"{OWM_ENDPOINT}", params=owm_params))
-res.raise_for_status()
-owm_data = res.json()
-print((owm_data))
+def owm_tracker():
+    res = (requests.get(f"{OWM_ENDPOINT}", params=OWM_PARAMS))
+    res.raise_for_status()
+    owm_data = res.json()
+    rainy = False
+    for hour in owm_data["hourly"]:
+        if owm_data["hourly"].index(hour) in range(0,12):
+            if hour["weather"][0]["id"] < 700:
+                rainy = True
+    if rainy == True:
+        print("Bring an umbrella.")
+
+
+owm_tracker()
