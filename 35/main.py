aa1df95748210600a7ee999f6c45fe7d58ca72fa
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-27 15:44:31 -0500

    Twilio SID and Auth Token added to .env, imported to Day 35

--- 35/main.py
+++ 35/main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 import requests
+from twilio.rest import Client
 
 load_dotenv(find_dotenv())
 
@@ -11,18 +12,16 @@ OWM_PARAMS = {
     "appid": os.environ.get("WEATHER_API_KEY"),
     "exclude": "current,minutely,daily"
 }
+ACCT_SID = os.environ.get("TWILIO_ACCT_SID")
+AUTH_TOKEN = os.environ.get("TWILIO_ACCT_TOKEN")
 
 def owm_tracker():
     res = (requests.get(f"{OWM_ENDPOINT}", params=OWM_PARAMS))
     res.raise_for_status()
-    owm_data = res.json()
     rainy = False
-    for hour in owm_data["hourly"]:
-        if owm_data["hourly"].index(hour) in range(0,12):
-            if hour["weather"][0]["id"] < 700:
-                rainy = True
-    if rainy == True:
-        print("Bring an umbrella.")
+    for hour in res.json()["hourly"][:12]:
+        if hour["weather"][0]["id"] < 700:
+            rainy = True
+    if rainy == True: print("Bring an umbrella.")
 
-
-owm_tracker()
+# owm_tracker()
