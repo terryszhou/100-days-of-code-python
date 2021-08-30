10bee0bb701f2940b87ac5ad89783e9ee5218e80
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-30 12:51:37 -0500

    Day 36 Step Two complete.

--- 36/main.py
+++ 36/main.py
@@ -1,23 +1,33 @@
 # - - - - - - - - - - IMPORT MODULES - - - - - - - - - - #
 import os
+import html
 from dotenv import load_dotenv, find_dotenv
 import requests
 load_dotenv(find_dotenv())
 
 # - - - - - - - - - - SETUP VARIABLES - - - - - - - - - - #
-STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
-STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-API_URL = "https://www.alphavantage.co/query?"
-API_PARAMS = {
+STOCK = "TSLA"
+STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
+STOCK_URL = "https://www.alphavantage.co/query?"
+STOCK_PARAMS = {
     "function": "TIME_SERIES_DAILY_ADJUSTED",
     "symbol": STOCK,
     "apikey": STOCK_API_KEY
 }
 
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+NEWS_URL = "https://newsapi.org/v2/everything?"
+# NEWS_PARAMS = {
+#     "q": COMPANY_NAME,
+#     "from": last_two_days[1][0],
+
+
+# }
+
 # - - - - - - - - - - STOCK FUNCTION - - - - - - - - - - #
 def stock_checker():
-    res = requests.get(API_URL, params=API_PARAMS).json()
+    res = requests.get(STOCK_URL, params=STOCK_PARAMS).json()
     last_two_days = list(res["Time Series (Daily)"].items())[:2]
     l2d_closing = []
     for day in last_two_days:
@@ -27,7 +37,8 @@ def stock_checker():
     percent_change = difference / l2d_closing[1]
     if percent_change > .05:
         print("Get News")
-    
+        news_res = requests.get(f"https://newsapi.org/v2/everything?q={COMPANY_NAME}&from={last_two_days[1][0]}&sortBy=publishedAt&apiKey={NEWS_API_KEY}").json()
+        news_articles = news_res["articles"]
 
 stock_checker()
 
