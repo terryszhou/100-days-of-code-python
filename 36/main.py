88e4aa46d10cbd28c51565e3620a44dc9dda9ece
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-30 12:17:17 -0500

    Day 36 Stock checker complete through Step 1.

--- 36/main.py
+++ 36/main.py
@@ -1,13 +1,13 @@
+# - - - - - - - - - - IMPORT MODULES - - - - - - - - - - #
 import os
 from dotenv import load_dotenv, find_dotenv
 import requests
-
 load_dotenv(find_dotenv())
 
+# - - - - - - - - - - SETUP VARIABLES - - - - - - - - - - #
 STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
-
 API_URL = "https://www.alphavantage.co/query?"
 API_PARAMS = {
     "function": "TIME_SERIES_DAILY_ADJUSTED",
@@ -15,7 +15,21 @@ API_PARAMS = {
     "apikey": STOCK_API_KEY
 }
 
-res = requests.get(API_URL, params=API_PARAMS)
+# - - - - - - - - - - STOCK FUNCTION - - - - - - - - - - #
+def stock_checker():
+    res = requests.get(API_URL, params=API_PARAMS).json()
+    last_two_days = list(res["Time Series (Daily)"].items())[:2]
+    l2d_closing = []
+    for day in last_two_days:
+        close_price = float(day[1]["4. close"])
+        l2d_closing.append(close_price)
+    difference = abs(l2d_closing[0] - l2d_closing[1])
+    percent_change = difference / l2d_closing[1]
+    if percent_change > .05:
+        print("Get News")
+    
+
+stock_checker()
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
