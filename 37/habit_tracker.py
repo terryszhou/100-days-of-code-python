72481c48b62a067302de0a8976ac08ff84e16a3b
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-31 10:17:21 -0500

    Day 37 Pixel put requests complete.

--- 37/habit_tracker.py
+++ 37/habit_tracker.py
@@ -1,12 +1,15 @@
 # - - - - - - - - - - IMPORT MODULES - - - - - - - - - - #
 import requests
 import os
+from datetime import datetime
 from dotenv import load_dotenv, find_dotenv
+from requests.api import head
 load_dotenv(find_dotenv())
 
 # - - - - - - - - - - SETUP VARIABLES - - - - - - - - - - #
 TOKEN = os.environ.get("PIXELA_ACCT_TOKEN")
 USERNAME = "terryszhou"
+TODAY = datetime.now().strftime("%Y%m%d")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
@@ -27,9 +30,13 @@ graph_params = {
 
 pixel_endpoint = f"{graph_endpoint}/{graph_params['id']}"
 pixel_params = {
-    "date": "20210830",
+    "date": TODAY,
     "quantity": "100",
     "optionalData": "{\"rep/set max\":\"25\"}"
+}
+
+put_params = {
+    "quantity": "50"
 }
 
 headers = {
@@ -53,5 +60,13 @@ def post_pixel():
     response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
     print(response.text)
 
-post_pixel()
+# post_pixel()
+
+def put_pixel():
+    response=requests.put(f"{pixel_endpoint}/{TODAY}", json=put_params, headers=headers)
+    print(response.text)
+
+# put_pixel()
+
+
 
