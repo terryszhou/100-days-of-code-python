3277fff2918ca568c7510b2887f2b6395c48c934
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-31 13:55:15 -0500

    Day 38 workout info successfully posted to sheety excel endpoint.

--- 38/workout_tracker.py
+++ 38/workout_tracker.py
@@ -9,6 +9,8 @@ load_dotenv(find_dotenv())
 APP_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 APP_ID = os.environ.get("NUTRITIONIX_ID")
 APP_KEY = os.environ.get("NUTRITIONIX_API_KEY")
+TODAY = datetime.now().strftime("%d/%m/%Y")
+NOW = datetime.now().strftime("%H:%M:%S")
 req_body = {
     "query": "",
     "gender": "male",
@@ -16,10 +18,12 @@ req_body = {
     "height_cm": 177.8,
     "age": 27
 }
+
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": APP_KEY
 }
+SHEETY_ENDPOINT = "https://api.sheety.co/460f379ce0b4e690d10bdc36715fcddf/myWorkouts/workouts"
 
 # - - - - - - - - - - FUNCTIONS - - - - - - - - - - #
 def workout_tracker():
@@ -27,7 +31,46 @@ def workout_tracker():
     user_input = input("Tell me what exercises you did: ")
     req_body["query"] = user_input
     res = req.post(APP_ENDPOINT, req_body, headers=headers)
-    print(res.json())
+
+    workout_req_body = {
+        "workout": {
+            'date': TODAY,
+            'time': NOW,
+            'exercise': res.json()["exercises"][0]["user_input"],
+            'duration': res.json()["exercises"][0]["duration_min"],
+            'calories': res.json()["exercises"][0]["nf_calories"],
+        }
+    }
+
+    res_2 = req.post(SHEETY_ENDPOINT, json=workout_req_body)
+    print(res_2.text)
 
 workout_tracker()
 
+# - - - - - - - - - - SAMPLE JSON DATA FORMATS - - - - - - - - - - #
+{
+    'exercises': [{
+        'tag_id': 825,
+        'user_input': 'pullups',
+        'duration_min': 8.33,
+        'met': 3.8,
+        'nf_calories': 41.89,
+        'photo': {'highres': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/825_highres.jpg',
+        'thumb': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/825_thumb.jpg',
+        'is_user_uploaded': False},
+        'compendium_code': 2022,
+        'name': 'pull-up',
+        'description': None,
+        'benefits': None
+    }]
+}
+
+{
+    'workouts': {
+        'date': '21/07/2020',
+        'time': '15:00:00',
+        'exercise': 'Running',
+        'duration': 22,
+        'calories': 130,
+    }
+}
