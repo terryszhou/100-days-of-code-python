6ee7a903f63d4ffa53ccfa1869519aafe36a09cc
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-31 10:00:10 -0500

    Day 37 Pixela graph created.

--- 37/habit_tracker.py
+++ 37/habit_tracker.py
@@ -5,18 +5,37 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 # - - - - - - - - - - SETUP VARIABLES - - - - - - - - - - #
+TOKEN = os.environ.get("PIXELA_ACCT_TOKEN")
+USERNAME = "terryszhou"
 pixela_endpoint = "https://pixe.la/v1/users"
-token = os.environ.get("PIXELA_ACCT_TOKEN")
 user_params = {
-    "token": token,
-    "username": "terryszhou",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_params = {
+    "id": "graph1",
+    "name": "Pullups Graph",
+    "unit": "reps",
+    "type": "int",
+    "color": "shibafu"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
 # - - - - - - - - - - FUNCTION - - - - - - - - - - #
-def habit_tracker():
+def create_pixela_acct():
     response = requests.post(pixela_endpoint, json=user_params)
+    print(response.text)
+
+# create_pixela_acct()
+
+def habit_tracker():
+    response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
     print(response.text)
 
 habit_tracker()
