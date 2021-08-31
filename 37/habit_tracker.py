ed8cc16856a38fe708b0e2f7a1ddc6474c578920
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-08-31 10:08:56 -0500

    Day 37 pixel posting complete.

--- 37/habit_tracker.py
+++ 37/habit_tracker.py
@@ -7,6 +7,7 @@ load_dotenv(find_dotenv())
 # - - - - - - - - - - SETUP VARIABLES - - - - - - - - - - #
 TOKEN = os.environ.get("PIXELA_ACCT_TOKEN")
 USERNAME = "terryszhou"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 user_params = {
     "token": TOKEN,
@@ -23,19 +24,34 @@ graph_params = {
     "type": "int",
     "color": "shibafu"
 }
+
+pixel_endpoint = f"{graph_endpoint}/{graph_params['id']}"
+pixel_params = {
+    "date": "20210830",
+    "quantity": "100",
+    "optionalData": "{\"rep/set max\":\"25\"}"
+}
+
 headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-# - - - - - - - - - - FUNCTION - - - - - - - - - - #
+# - - - - - - - - - - FUNCTIONS - - - - - - - - - - #
 def create_pixela_acct():
     response = requests.post(pixela_endpoint, json=user_params)
     print(response.text)
 
 # create_pixela_acct()
 
-def habit_tracker():
+def create_pixela_graph():
     response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
     print(response.text)
 
-habit_tracker()
+# create_pixela_graph()
+
+def post_pixel():
+    response = requests.post(url=pixel_endpoint, json=pixel_params, headers=headers)
+    print(response.text)
+
+post_pixel()
+
