3ebc7c1595e742a9c9f635ab6e0aa8373d74171b
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-09-16 14:24:28 -0500

    Day 45 complete.

--- 45/main.py
+++ 45/main.py
@@ -1,19 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-response = requests.get("https://news.ycombinator.com/news")
-yc_web_page = response.text
+response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+empire_page = response.text
+soup = bs(empire_page, "html.parser")
 
-soup = bs(yc_web_page, "html.parser")
-articles = soup.find_all(name="a", class_="storylink")
-article_texts = [tag.getText() for tag in articles]
-article_links = [tag.get("href") for tag in articles]
+def movie_parser():
+    movie_list = []
+    movies = soup.find_all(class_="listicle-item")
+    for movie in movies:
+        movie_num = str(101 - int(movie.find(class_="listicle-item-count").getText().split()[0]))
+        movie_name = movie.find("img")["alt"]
+        movie_list.append(f"{movie_num}) {movie_name}")
 
-# for article_tag in articles:
-#     article_texts.append(article_tag.getText())
-#     article_links.append(article_tag.get("href"))
+    movie_list.reverse()
 
-article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
-top_article_index = article_upvotes.index(max(article_upvotes))
-top_article_text = article_texts[top_article_index]
-top_article_link = article_links[top_article_index]
+    with open("movies.txt", mode="w") as file:
+        for movie in movie_list:
+            file.write(f"{movie}\n")
+            
+    print("Movies parsed!")
+
+movie_parser()
